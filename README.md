7ef64a60289ff783a8bb470c1ff4cc1de0efdcf6
Zhengzhong Zhao <zhengzhongzhao@gmail.com>
2014-07-28 14:59:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+###应用场景
+当别人开始给你提bug的时候 你可能一天要向同样的repo同样的branch提n个pull request。
+用这个工具可以节省时间。
+
+###原理
+gitlab提供非常友好的URL，可以用URL指定branch repo提pullrequest.
+
+###用法
+在gitlab网站，创建一个merge request，点compare branch，观察下URL。你懂的。
+我个人就不关这个程序了，没弄数据持久。
+
+###TODO
+- 小豪想直接发个POST请求。
+- Maybe turn it to a status bar app?
+
+###用了之后个人感觉***爽矣***。
--- iPullRequest/AppDelegate.m
+++ iPullRequest/AppDelegate.m
@@ -8,11 +8,48 @@
 
 #import "AppDelegate.h"
 
+@interface AppDelegate()
+
+@property (weak) IBOutlet NSTextField *sourceBranchName;
+
+@property (weak) IBOutlet NSTextField *targetBranchName;
+
+@property (weak) IBOutlet NSTextField *sourceProjectID;
+
+@property (weak) IBOutlet NSTextField *destinationProjectID;
+
+@property (strong, nonatomic) NSMutableString *prURLPrefix;
+
+@property (weak) IBOutlet NSTextField *accountName;
+
+@end
+
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    // http://gitlab.corp.anjuke.com/zhengzhongzhao/Anjuke/merge_requests/new?utf8=%E2%9C%93&merge_request%5Bsource_project_id%5D=69&merge_request%5Bsource_branch%5D=zzhengzhong_7.6&merge_request%5Btarget_project_id%5D=24&merge_request%5Btarget_branch%5D=develop_i-anjuke_7.6
+    self.prURLPrefix = [@"http://gitlab.corp.anjuke.com/" mutableCopy];
+    // /Anjuke/merge_requests/new?utf8=%E2%9C%93&merge_request%5Bsource_project_id%5D=
+    
+}
+
+- (IBAction)openPR:(id)sender {
+    NSMutableString *prString;
+    [prString appendString:self.prURLPrefix];
+    [prString appendString:self.accountName.stringValue];
+    [prString appendString:@"/Anjuke/merge_requests/new?utf8=%E2%9C%93&merge_request%5Bsource_project_id%5D="];
+    [prString appendString:self.sourceProjectID.stringValue];
+    [prString appendString:@"&merge_request%5Bsource_branch%5D="];
+    [prString appendString:self.sourceBranchName.stringValue];
+    [prString appendString:@"&merge_request%5Btarget_project_id%5D="];
+    [prString appendString:self.destinationProjectID.stringValue];
+    [prString appendString:@"&merge_request%5Btarget_branch%5D="];
+    [prString appendString:self.targetBranchName.stringValue];
+//    prString = [@"http://gitlab.corp.anjuke.com/zhengzhongzhao/Anjuke/merge_requests/new?utf8=%E2%9C%93&merge_request%5Bsource_project_id%5D=69&merge_request%5Bsource_branch%5D=zzhengzhong_7.6&merge_request%5Btarget_project_id%5D=24&merge_request%5Btarget_branch%5D=develop_i-anjuke_7.6" mutableCopy];
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:prString]];
+
 }
 
 @end
